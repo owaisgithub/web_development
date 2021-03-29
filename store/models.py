65f5ec7648c9789c7d009f8d9b8ca47cede7b64e
Owais <owaismfos@gmail.com>
2021-03-29 20:48:45 +0530

    Create all required models

--- store/models.py
+++ store/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class ProductCategory(models.Model):
@@ -9,8 +17,6 @@ class ProductCategory(models.Model):
         return self.category_name
 
     
-
-
 class Product(models.Model):
     product_category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     product_image = models.ImageField(upload_to='pics')
@@ -22,10 +28,48 @@ class Product(models.Model):
         return self.product_name
 
 
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transation_id = models.CharField(max_length=12)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.product.name
+
+    def get_total(self):
+        total = self.product.product_price * self.quantity
+        return total
+
+
 class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
     mobile_number = models.IntegerField()
     address = models.TextField()
     city = models.CharField(max_length=100)
     pin = models.IntegerField()
+
+    def __str__(self):
+        return self.address
 
